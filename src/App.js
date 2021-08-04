374af802d4d325e4108cd830781f4eeb0e938957
Marco <antnyhern@gmail.com>
2021-08-03 20:27:13 -0500

    All proyect

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [form, setform] = useState({
+    input:"",
+  })
+
+  const handleChange = async (e) => {
+    const {name, value } = e.target;
+    await setform({
+      ...form,
+      [name]:value,
+    });
+  };
+
+  const copiarPortapaeles = () =>{
+    var aux = document.createElement("input");
+    aux.setAttribute("value", form.input);
+    document.body.appendChild(aux);
+    aux.select();
+    document.execCommand("copy");
+    document.body.removeChild(aux);
+
+    alert("Se copio al portapapele");
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input className="form-constrol" type="text" name="input" onChange={handleChange}/>
+      
+      <button className="btn btn-primary" onClick={copiarPortapaeles}>  Copiar</button>
     </div>
   );
 }
